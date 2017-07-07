2086be3aac94979ab933e63c94c423406c5c9d5c
kandelvijaya <kandelvijaya@gmail.com>
2017-07-07 23:23:19 +0200

    Configured project

--- Build/Intermediates/ResultType.build/Debug-iphonesimulator/ResultType.build/DerivedSources/ResultType_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ResultTypeVersionString[];
- extern const double ResultTypeVersionNumber;
-
- const unsigned char ResultTypeVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ResultType  PROJECT:ResultType-1" "\n";
- const double ResultTypeVersionNumber __attribute__ ((used)) = (double)1.;
--- Build/Intermediates/ResultType.build/Debug-iphonesimulator/ResultTypeTests.build/DerivedSources/ResultTypeTests_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char ResultTypeTestsVersionString[];
- extern const double ResultTypeTestsVersionNumber;
-
- const unsigned char ResultTypeTestsVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:ResultTypeTests  PROJECT:ResultType-1" "\n";
- const double ResultTypeTestsVersionNumber __attribute__ ((used)) = (double)1.;
